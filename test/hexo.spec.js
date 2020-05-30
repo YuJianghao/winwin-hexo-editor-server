04bcd4cfa21dc0f6817c8f15ae48c78ade22f95f
winwin <>
2020-05-30 17:52:53 +0800

    add example blog

--- test/hexo.spec.js
+++ test/hexo.spec.js
@@ -1,37 +1,15 @@
 /* eslint-disable no-undef */
 const Hexo = require('../src/hexo')
 describe('check existence of hexo blog', () => {
-  test('not a npm folder', async () => {
-    var err = null
-    try {
-      const hexo = new Hexo('/')
-      err = await hexo._init()
-    } catch (e) {
-      err = e
-    } finally {
-      expect(err.message).toMatch('isn\'t a hexo blog folder!')
-    }
-  })
   test('not a hexo blog folder', async () => {
     var err = null
     try {
-      const hexo = new Hexo('./')
-      err = await hexo._init()
+      const hexo = new Hexo()
+      err = await hexo.init('/')
     } catch (e) {
       err = e
     } finally {
       expect(err.message).toMatch('isn\'t a hexo blog folder!')
     }
-  })
-})
-
-describe('check is git repository', () => {
-  test('/', () => {
-    const hexo = new Hexo('/')
-    expect(hexo.isGit).toBe(false)
-  })
-  test('./', () => {
-    const hexo = new Hexo('./')
-    expect(hexo.isGit).toBe(true)
   })
 })
